786a8b2a95737d2883cb77fce6bf4c6aa57ed991
azariah15yocoder <easey.boys@gmail.com>
2025-04-03 20:31:04 -0500

    More changes
    This might allow the final count to stay centered.

--- seaCross/scripts/drawingFunctions.js
+++ seaCross/scripts/drawingFunctions.js
@@ -98,6 +98,8 @@ colorRect(pauseResumeX + 15, pauseResumeY + 5, 5, 20, 'white');
 
 async function drawAll() {
     var spaces = 0;
+    var timesPlayedSpaces = 0;
+    var timesPlayedString = 'times played: ' + timesPlayed;
     var highScoreAsArray = highScore.toString().split('');
     var powerup;
     var powerupThumbnail;
@@ -110,6 +112,7 @@ async function drawAll() {
         highScoreAsArray.pop();
     }
     spaces = 17.3 * highScoreAsArray.length;
+    timesPlayedSpaces = 17.3 * timesPlayedString.length;
 
     if(allFishLoaded && 
         crabImage1Loaded && crabImage2Loaded && crabImage3Loaded && pinkShellLoaded && blueShellLoaded && bonusShellFrame1Loaded && bonusShellFrame2Loaded && allIsraeliteImagesLoaded && allEgyptianImagesLoaded && allPowerupsLoaded && mosesImagesLoaded && allQuestionsLoaded && rebootsLoaded && backgroundScoreLoaded && frequency >= numberOfCases && !confirmed) {
@@ -724,7 +727,7 @@ async function drawAll() {
         canvasContext.drawImage(israelites[2].images[israelites[2].imageId], canvas.width / 2, canvas.height / 2 + 50 , 35, 35);
         canvasContext.drawImage(egyptians[0].images[egyptians[0].imageId], canvas.width / 2 - 50, canvas.height / 2 + 50 , 35, 35);
         canvasContext.drawImage(israelites[0].images[israelites[0].imageId], canvas.width / 2 - 100, canvas.height / 2 + 50 , 35, 35);
-        arcadeMode ? printText(265, 500, 28, 'white', 'times played: ' + timesPlayed) : false;
+        arcadeMode ? printText(265 - timesPlayedSpaces, 500, 28, 'white', timesPlayedString) : false;
         printText(20, 30, 28, 'white', 'score');
         printText(20, 60, 28, 'white', zeroedScore(score));
         printText(canvas.width - 193, 30, 28, 'white', 'high score');
