1201ce1c46d8109e4f13b282564777bf44a8b953
azariah15yocoder <easey.boys@gmail.com>
2026-05-25 11:16:12 -0500

    Update drawingFunctions.js

--- campMeeting/scripts/drawingFunctions.js
+++ campMeeting/scripts/drawingFunctions.js
@@ -817,8 +817,8 @@ async function drawAll() {
     }
 
     printText(265 - timesPlayedSpaces, 60, 28, 'white', 'times played: ' + timesPlayed);
-		printText(295 - timesPlayedSpaces, 530, 28, 'white', "a. english");
-		printText(295 - timesPlayedSpaces, 570, 28, 'white', "b. español");
+		printText(300 - timesPlayedSpaces, 530, 28, 'white', "a. english");
+		printText(300 - timesPlayedSpaces, 570, 28, 'white', "b. español");
         printText(20, 30, 28, 'white', 'score');
         printText(20, 60, 28, 'white', zeroedScore(score));
         printText(canvas.width - 193, 30, 28, 'white', 'high score');
